2228a655e548e317a9fbcec2ef93739601a2ba6e
yjs06070 <yjs06070@gmail.com>
2020-03-25 18:58:58 +0900

    initial commit

---
+++ hw/sum1.js
@@ -0,0 +1,6 @@
+let sum=0;
+let i=0;
+for(i=0;i<56;i++){
+    sum+=i;
+}
+console.log(sum);
---
+++ hw/sum2.js
@@ -0,0 +1,7 @@
+let i=0;
+let sum=0;
+for(i=0;i<56;i++){
+    if(i%3==0){
+    sum+=i;}
+}
+console.log(sum);
