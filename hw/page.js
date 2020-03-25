364e3195f4d37aead6bf3e322cb74c04a17dd4be
yjs06070 <yjs06070@gmail.com>
2020-03-26 00:35:00 +0900

    initial commit

---
+++ hw/page.js
@@ -0,0 +1,7 @@
+let studentCount =31;
+let pageSize =10;
+let n=Math.floor(studentCount/pageSize);
+if(studentCount%pageSize!=0) {
+    n=n+1;
+}
+console.log(n);
---
+++ hw/random.js
@@ -0,0 +1,4 @@
+for(let i=0;i<=30;i++){
+i=Math.floor(Math.random()*20+10);
+console.log(i);
+}
