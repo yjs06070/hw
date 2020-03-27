7b08096bb1a2ba8ceabd9d2403d48693f76a2ab4
yjs06070 <yjs06070@gmail.com>
2020-03-27 22:32:55 +0900

    initial commit

--- hw/page.js
+++ hw/page.js
@@ -4,4 +4,4 @@ let n=Math.floor(studentCount/pageSize);
 if(studentCount%pageSize!=0) {
     n=n+1;
 }
-console.log(n);
+console.log(n); 
--- hw/random.js
+++ hw/random.js
@@ -1,4 +1,4 @@
 for(let i=0;i<30;++i){
 let n= Math.floor((Math.random()*10)+10);
-console.log(n);
+console.log(n); 
 }
